cde1a4a38fa8262ecb1fe72993e41c8152454ad2
baberldn <berilbadan1903@gmail.com>
2025-02-10 12:29:53 +0300

    we

--- page.js
+++ page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import "./App.css"
 
 export default function Login() {
   const router = useRouter();
